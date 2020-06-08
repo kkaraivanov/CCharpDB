c337175fda557fc98e6418dcc3027339c7a85e4c
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-08 21:11:01 +0300

    delete question

--- DataAggregation/SalaryChallenge.sql
+++
@@ -1,5 +0,0 @@
-WITH temp AS(SELECT e.DepartmentId Id, AVG(e.Salary) salary FROM Employees e
-	GROUP BY e.DepartmentId)
-SELECT TOP(10) e.FirstName, e.LastName, e.DepartmentID FROM Employees e, temp t
-WHERE e.DepartmentID = t.Id AND e.Salary > t.salary
-ORDER BY e.DepartmentID
