8bac4ae8e9a5ed18c157df5868fb3e5e7c3a3cc0
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-15 22:07:21 +0300

    update

--- DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
+++ DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
@@ -279,6 +279,148 @@ BEGIN
 END
 
 /* Problem 19. Trigger */
-select* from UsersGames
+CREATE TRIGGER tr_ItemsPurchaseRestrictions ON UserGameItems FOR INSERT
+AS
+BEGIN
+  DECLARE @userGameLevel INT = (
+    SELECT ug.Level
+    FROM inserted AS ugi
+    JOIN UsersGames AS ug ON ugi.UserGameId = ug.Id
+  )
+  DECLARE @itemMinLevel int = (
+    SELECT i.MinLevel
+    FROM inserted AS ugi
+    JOIN Items AS i on i.Id = ugi.ItemId
+  )
+  IF(@itemMinLevel > @userGameLevel)
+    BEGIN
+      ROLLBACK;
+      RAISERROR('Higher user game level required for item purchase', 16, 1);
+      RETURN;
+    END
+END
 
-select* from Items
+/* Problem 20. *Massive Shopping */
+DECLARE @gameName nvarchar(50) = 'Safflower';
+DECLARE @username nvarchar(50) = 'Stamat';
+
+DECLARE @userGameId int = (
+  SELECT ug.Id 
+  FROM UsersGames AS ug
+  JOIN Users AS u ON ug.UserId = u.Id
+  JOIN Games AS g ON ug.GameId = g.Id
+  WHERE u.Username = @username AND g.Name = @gameName
+
+);
+
+DECLARE @userGameLevel int = (SELECT Level FROM UsersGames WHERE Id = @userGameId);
+DECLARE @itemsCost money, @availableCash money, @minLevel int, @maxLevel int;
+
+SET @minLevel = 11; SET @maxLevel = 12;
+SET @availableCash = (SELECT Cash FROM UsersGames WHERE Id = @userGameId);
+SET @itemsCost = (SELECT SUM(Price) FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel);
+
+IF (@availableCash >= @itemsCost AND @userGameLevel >= @maxLevel) 
+
+BEGIN 
+  BEGIN TRANSACTION  
+  UPDATE UsersGames SET Cash -= @itemsCost WHERE Id = @userGameId; 
+  IF(@@ROWCOUNT <> 1) 
+  BEGIN
+    ROLLBACK;
+	RAISERROR('Could not make payment', 16, 1)
+  END
+
+  ELSE
+  BEGIN
+    INSERT INTO UserGameItems (ItemId, UserGameId) 
+    (SELECT Id, @userGameId FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel) 
+
+    IF((SELECT COUNT(*) FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel) <> @@ROWCOUNT)
+    BEGIN
+      ROLLBACK;
+	  RAISERROR('Could not buy items', 16, 1)
+    END	
+	COMMIT;
+  END
+END
+
+SET @minLevel = 19; SET @maxLevel = 21;
+SET @availableCash = (SELECT Cash FROM UsersGames WHERE Id = @userGameId);
+SET @itemsCost = (SELECT SUM(Price) FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel);
+
+IF (@availableCash >= @itemsCost AND @userGameLevel >= @maxLevel) 
+
+BEGIN 
+  BEGIN TRANSACTION  
+  UPDATE UsersGames SET Cash -= @itemsCost WHERE Id = @userGameId; 
+
+  IF(@@ROWCOUNT <> 1) 
+  BEGIN
+    ROLLBACK;
+	RAISERROR('Could not make payment', 16, 1)
+  END
+
+  ELSE
+  BEGIN
+    INSERT INTO UserGameItems (ItemId, UserGameId) 
+    (SELECT Id, @userGameId FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel) 
+
+    IF((SELECT COUNT(*) FROM Items WHERE MinLevel BETWEEN @minLevel AND @maxLevel) <> @@ROWCOUNT)
+    BEGIN
+      ROLLBACK;
+	  RAISERROR('Could not buy items', 16, 1)
+    END	
+
+    ELSE COMMIT;
+  END
+END
+
+SELECT i.Name AS [Item Name]
+FROM UserGameItems AS ugi
+JOIN Items AS i ON i.Id = ugi.ItemId
+JOIN UsersGames AS ug ON ug.Id = ugi.UserGameId
+JOIN Games AS g ON g.Id = ug.GameId
+WHERE g.Name = @gameName
+ORDER BY [Item Name]
+
+/* Problem 21. Employees with Three Projects */
+CREATE PROC usp_AssignProject(@EmployeeId INT, @ProjectId INT)
+AS
+BEGIN
+	BEGIN TRANSACTION
+		INSERT INTO EmployeesProjects(EmployeeID, ProjectID) VALUES
+		(@EmployeeId, @ProjectId)
+
+		DECLARE @numberOfProjects INT = 
+		(SELECT COUNT(ep.ProjectID) FROM Employees AS e
+		JOIN EmployeesProjects AS ep ON ep.EmployeeID =  e.EmployeeID
+		WHERE e.EmployeeID = @EmployeeId)
+
+		IF(@numberOfProjects > 3)
+		BEGIN
+			ROLLBACK
+			RAISERROR('The employee has too many projects!', 16, 1)
+			RETURN
+		END
+
+	COMMIT
+END
+
+/* Problem 22. Delete Employees */
+CREATE TABLE Deleted_Employees(
+	EmployeeId INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(50) NOT NULL,
+	JobTitle NVARCHAR(50) NOT NULL,
+	DepartmentId INT FOREIGN KEY REFERENCES Departments(DepartmentID),
+	Salary DECIMAL(15, 2) NOT NULL
+)
+
+CREATE TRIGGER tr_DeleteEmployees ON Employees AFTER DELETE
+AS
+BEGIN
+	INSERT INTO Deleted_Employees(FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+	SELECT d.FirstName, d.LastName, d.MiddleName, d.JobTitle, d.DepartmentID, d.Salary FROM Deleted d
+END
