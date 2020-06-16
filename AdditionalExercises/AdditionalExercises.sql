475a9bc5e733df0231bdd1f4c740854ac2b00cbd
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-16 13:05:50 +0300

    complete Additional Exercises

--- AdditionalExercises/AdditionalExercises.sql
+++ AdditionalExercises/AdditionalExercises.sql
@@ -155,4 +155,86 @@ RIGHT JOIN Currencies cu ON cu.CurrencyCode = c.CurrencyCode
 GROUP BY cu.CurrencyCode, cu.Description
 ORDER BY [NumberOfCountries] DESC, [Currency] ASC
 
-SELECT * FROM Countries
+/* Problem 12.	Population and Area by Continent */
+SELECT * FROM Continents;
+
+SELECT co.ContinentName,
+	SUM(c.AreaInSqKm) [CountriesArea],
+	SUM(CAST(c.[Population] AS FLOAT)) [CountriesPopulation]
+	FROM Countries c
+JOIN Continents co ON c.ContinentCode = co.ContinentCode
+GROUP BY co.ContinentName
+ORDER BY CountriesPopulation DESC
+
+/* Problem 13.	Monasteries by Country */
+CREATE TABLE Monasteries(
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	[Name] NVARCHAR(50),
+	CountryCode CHAR(2) FOREIGN KEY REFERENCES Countries(CountryCode)
+	)
+
+INSERT INTO Monasteries(Name, CountryCode) VALUES
+('Rila Monastery “St. Ivan of Rila”', 'BG'), 
+('Bachkovo Monastery “Virgin Mary”', 'BG'),
+('Troyan Monastery “Holy Mother''s Assumption”', 'BG'),
+('Kopan Monastery', 'NP'),
+('Thrangu Tashi Yangtse Monastery', 'NP'),
+('Shechen Tennyi Dargyeling Monastery', 'NP'),
+('Benchen Monastery', 'NP'),
+('Southern Shaolin Monastery', 'CN'),
+('Dabei Monastery', 'CN'),
+('Wa Sau Toi', 'CN'),
+('Lhunshigyia Monastery', 'CN'),
+('Rakya Monastery', 'CN'),
+('Monasteries of Meteora', 'GR'),
+('The Holy Monastery of Stavronikita', 'GR'),
+('Taung Kalat Monastery', 'MM'),
+('Pa-Auk Forest Monastery', 'MM'),
+('Taktsang Palphug Monastery', 'BT'),
+('Sümela Monastery', 'TR')
+
+ALTER TABLE Countries
+ADD IsDeleted BIT NULL
+CONSTRAINT Countries_IsDeleted DEFAULT(0);
+GO
+UPDATE Countries
+SET IsDeleted = 0;
+GO
+ALTER TABLE Countries
+ALTER COLUMN IsDeleted BIT NOT NULL;
+GO
+
+UPDATE Countries
+SET IsDeleted = 1
+WHERE CountryCode IN (
+	SELECT c.CountryCode
+	FROM Countries c
+	JOIN CountriesRivers cr ON c.CountryCode = cr.CountryCode
+	JOIN Rivers r ON cr.RiverId = r.Id
+	GROUP BY c.CountryCode
+	HAVING COUNT(r.Id) > 3)
+
+SELECT m.Name [Monastery], c.CountryName [Country] FROM Monasteries m
+JOIN Countries c ON m.CountryCode = c.CountryCode
+WHERE c.IsDeleted = 0
+ORDER BY Monastery;
+
+/* Problem 14.	Monasteries by Continents and Countries */
+UPDATE Countries
+SET CountryName = 'Burma'
+WHERE CountryName = 'Myanmar'
+
+INSERT INTO Monasteries(Name, CountryCode) 
+SELECT 'Hanga Abbey', CountryCode FROM Countries WHERE CountryName = 'Tanzania'
+INSERT INTO Monasteries(Name, CountryCode) 
+SELECT 'Myin-Tin-Daik', CountryCode FROM Countries WHERE CountryName = 'Myanmar'
+
+SELECT co.ContinentName [ContinentName],
+	c.CountryName [CountryName],
+	COUNT(m.Id) [MonasteriesCount]
+	FROM Continents co
+JOIN Countries c ON co.ContinentCode = c.ContinentCode
+LEFT JOIN Monasteries m ON c.CountryCode = m.CountryCode
+WHERE c.IsDeleted = 0
+GROUP BY co.ContinentName, c.CountryName
+ORDER BY MonasteriesCount DESC, CountryName ASC
