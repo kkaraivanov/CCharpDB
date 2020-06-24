56074abd256180abe37367576cc5fc36d395c4f6
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-24 10:56:18 +0300

    new class DatabaseCreator

---
+++ EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/DatabaseCreator.cs
@@ -0,0 +1,179 @@
+﻿namespace IntroductionToDBApps
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using Microsoft.Data.SqlClient;
+
+    public class DatabaseCreator
+    {
+        /// <summary>
+        /// private filds used in DatabaseCreator Class
+        /// </summary>
+        private SqlDataReader reader = null;
+        private SqlConnection conn = null;
+        private SqlCommand command = null;
+        private string userName = null;
+        private string password = null;
+        private string sqlServer = null;
+        private string dataBase = null;
+        private string connectionString = null;
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="sqlServer">This is server name for example - localhost\serverName if there exists</param>
+        /// <param name="dataBase">This is database name</param>
+        public DatabaseCreator(string sqlServer, string dataBase)
+        {
+            this.sqlServer = sqlServer;
+            this.dataBase = dataBase;
+            this.connectionString = $"Server={sqlServer}; Database=master; Integrated Security=true";
+        }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="sqlServer">This is server name for example - localhost\serverName if there exists</param>
+        /// <param name="dataBase">This is database name</param>
+        /// <param name="userName">user name for access to MSSQL Server</param>
+        /// <param name="password">password for access to MSSQL Server if there exists</param>
+        public DatabaseCreator(string sqlServer, string dataBase, string userName, string password)
+            :this(sqlServer, dataBase)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.connectionString = $"Server={sqlServer}; Database=master; User Id={userName}; Password={password}";
+        }
+
+        /// <summary>
+        /// Return Server name from fild
+        /// </summary>
+        public string SqlServer => this.sqlServer;
+
+        /// <summary>
+        /// Return Database name from fild
+        /// </summary>
+        public string BaseName => this.dataBase;
+
+        /// <summary>
+        /// Database creation method
+        /// </summary>
+        public void CreateDatabase()
+        {
+            string sqlCreate = $"CREATE DATABASE {this.dataBase}";
+            
+            if (!DataBaseExist())
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+
+                using (conn = new SqlConnection(this.connectionString))
+                {
+                    conn.Open();
+                    command = new SqlCommand(sqlCreate, conn);
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ae)
+                    {
+                        Console.WriteLine(ae.Message.ToString());
+                    }
+                }
+            }
+            if (DataBaseExist())
+                this.connectionString = 
+                    this.userName == null ? $"Server={this.sqlServer}; Database={this.BaseName}; Integrated Security=true" :
+                    $"Server={this.sqlServer}; Database={this.BaseName}; User Id={this.userName}; Password={this.password}";
+        }
+
+        /// <summary>
+        /// Tables creation method
+        /// </summary>
+        /// <param name="tablesColection">Collection with table structure</param>
+        public void CreateTables(Dictionary<string, string> tablesColection)
+        {
+            if (DataBaseExist())
+            {
+                if (tablesColection.Count > 0)
+                {
+                    foreach (var (tableName, fildValue) in tablesColection)
+                    {
+                        CreateTable(tableName, fildValue);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The method checks if the database exists
+        /// </summary>
+        /// <returns>True if it exists. False if it does not exist.</returns>
+        private bool DataBaseExist()
+        {
+            string connectionStr = this.userName == null ? $"Server={sqlServer}; Database=master; Integrated Security=true" : 
+                $"Server={this.sqlServer}; Database=master; User Id = {this.userName}; Password={this.password}";
+            string checkDatabaseName = $"SELECT top(1) name FROM master.dbo.sysdatabases where name='{this.dataBase}'";
+            bool result = false;
+
+            using (conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                var currentCommand = new SqlCommand(checkDatabaseName, conn);
+                var currentReader = currentCommand.ExecuteReader();
+                string dbName = null;
+
+                while (currentReader.Read())
+                {
+                    for (int i = 0; i < currentReader.FieldCount; i++)
+                    {
+                        dbName = (string)currentReader[i];
+                    }
+                }
+
+                if (dbName != null)
+                    result = true;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Performs connection with MSSQL Server
+        /// </summary>
+        /// <param name="sql">Query to the server</param>
+        private void ExecuteSQLStatement(string sql)
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+
+            using (conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                command = new SqlCommand(sql, conn);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ae)
+                {
+                    Console.WriteLine(ae.Message.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Auxiliary method
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="filds"></param>
+        private void CreateTable(string tableName, string filds)
+        {
+            string tableString = $"CREATE TABLE {tableName}({filds})";
+            ExecuteSQLStatement(tableString);
+        }
+    }
+}
--- EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/StartUp.cs
+++ EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/StartUp.cs
@@ -1,142 +1,76 @@
 ﻿namespace IntroductionToDBApps
 {
     using System;
-    using System.Data;
-    using Microsoft.Data.SqlClient;
+    using System.Collections.Generic;
 
     class StartUp
     {
-        private static string connectionString = "Server=.\\SQLKARAIVANOV; Database=MinionsDB; User Id = user; Password=password";
-        private static SqlDataReader reader = null;
-        private static SqlConnection conn = null;
-        private static SqlCommand command = null;
-
         static void Main(string[] args)
         {
-            CreateDB();
-            CreateDbTables();
-
+            var dataBaseCreator = 
+                new DatabaseCreator(".\\SQLKARAIVANOV", "MinionsDB","user", "password");
+            dataBaseCreator.CreateDatabase();
+            var dataBaseTables = new Dictionary<string, string>();
+            dataBaseTables = Tables();
+            dataBaseCreator.CreateTables(dataBaseTables);
             
+
         }
 
-        private static void ExecuteSQLStatement(string sql)
+        private static Dictionary<string, string> Tables()
         {
-            if (conn.State == ConnectionState.Open)
-                conn.Close();
-
-            using (conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-                command = new SqlCommand(sql, conn);
-
-                try
-                {
-                    command.ExecuteNonQuery();
-                }
-                catch (SqlException ae)
-                {
-                    Console.WriteLine(ae.Message.ToString());
-                }
-            }
-        }
-
-        static void CreateDbTables()
-        {
+            var dataBaseTables = new Dictionary<string, string>();
             string tableName = null;
             string filds = null;
 
             tableName = "Countries";
             filds = "Id INT PRIMARY KEY IDENTITY," +
                     "Name VARCHAR(50)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
             tableName = "Towns";
             filds = "Id INT PRIMARY KEY IDENTITY," +
                     "Name VARCHAR(50)," +
                     "CountryCode INT FOREIGN KEY REFERENCES Countries(Id)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
             tableName = "Minions";
             filds = "Id INT PRIMARY KEY IDENTITY," +
                     "Name VARCHAR(30)," +
                     "Age INT," +
                     "TownId INT FOREIGN KEY REFERENCES Towns(Id)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
             tableName = "EvilnessFactors";
             filds = "Id INT PRIMARY KEY IDENTITY," +
                     "Name VARCHAR(50)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
             tableName = "Villains";
             filds = "Id INT PRIMARY KEY IDENTITY," +
                     "Name VARCHAR(50)," +
                     "EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
             tableName = "MinionsVillains";
             filds = "MinionId INT FOREIGN KEY REFERENCES Minions(Id)," +
                     "VillainId INT FOREIGN KEY REFERENCES Villains(Id)," +
                     "CONSTRAINT PK_MinionsVillains PRIMARY KEY (MinionId, VillainId)";
-            CreateTable(tableName, filds);
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
 
-            Console.WriteLine("All tables is Created");
-        }
-
-        private static void CreateTable(string tableName, string filds)
-        {
-            string tableString = $"CREATE TABLE {tableName}({filds})";
-            ExecuteSQLStatement(tableString);
-        }
-
-        private static void CreateDB()
-        {
-            string conString = "Server=.\\SQLKARAIVANOV; Database=master; User Id = user; Password=password";
-            string dbCheck = "SELECT top(1) name FROM master.dbo.sysdatabases where name='MinionsDB'";
-            string sqlCreate = "CREATE DATABASE MinionsDB";
-            bool DbNotExist = false;
-
-            using (conn = new SqlConnection(conString))
-            {
-                conn.Open();
-                command = new SqlCommand(dbCheck, conn);
-                reader = command.ExecuteReader();
-                string str = null;
-
-                while (reader.Read())
-                {
-                    for (int i = 0; i < reader.FieldCount; i++)
-                    {
-                        str = (string) reader[i];
-                    }
-                }
-
-                if (str == null)
-                    DbNotExist = true;
-            }
-
-            if (DbNotExist)
-            {
-                if (conn.State == ConnectionState.Open)
-                    conn.Close();
-
-                using (conn = new SqlConnection(conString))
-                {
-                    conn.Open();
-                    command = new SqlCommand(sqlCreate, conn);
-
-                    try
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                    catch (SqlException ae)
-                    {
-                        Console.WriteLine(ae.Message.ToString());
-                    }
-                }
-            }
-            else
-                return;
+            return dataBaseTables;
         }
     }
 }
