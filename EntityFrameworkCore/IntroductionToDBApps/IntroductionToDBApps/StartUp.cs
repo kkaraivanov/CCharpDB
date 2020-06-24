94c1cab9bd227700443f9d0f973c5e422e776e78
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-06-24 11:04:56 +0300

    update code

--- EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/StartUp.cs
+++ EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/StartUp.cs
@@ -7,70 +7,14 @@
     {
         static void Main(string[] args)
         {
-            var dataBaseCreator = 
-                new DatabaseCreator(".\\SQLKARAIVANOV", "MinionsDB","user", "password");
+            var dataBaseCreator = new DatabaseCreator(".\\SQLKARAIVANOV", "MinionsDB","user", "password");
+            var dataBaseTables = TablesPattern.Tables();
             dataBaseCreator.CreateDatabase();
-            var dataBaseTables = new Dictionary<string, string>();
-            dataBaseTables = Tables();
             dataBaseCreator.CreateTables(dataBaseTables);
             
 
         }
 
-        private static Dictionary<string, string> Tables()
-        {
-            var dataBaseTables = new Dictionary<string, string>();
-            string tableName = null;
-            string filds = null;
-
-            tableName = "Countries";
-            filds = "Id INT PRIMARY KEY IDENTITY," +
-                    "Name VARCHAR(50)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            tableName = "Towns";
-            filds = "Id INT PRIMARY KEY IDENTITY," +
-                    "Name VARCHAR(50)," +
-                    "CountryCode INT FOREIGN KEY REFERENCES Countries(Id)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            tableName = "Minions";
-            filds = "Id INT PRIMARY KEY IDENTITY," +
-                    "Name VARCHAR(30)," +
-                    "Age INT," +
-                    "TownId INT FOREIGN KEY REFERENCES Towns(Id)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            tableName = "EvilnessFactors";
-            filds = "Id INT PRIMARY KEY IDENTITY," +
-                    "Name VARCHAR(50)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            tableName = "Villains";
-            filds = "Id INT PRIMARY KEY IDENTITY," +
-                    "Name VARCHAR(50)," +
-                    "EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            tableName = "MinionsVillains";
-            filds = "MinionId INT FOREIGN KEY REFERENCES Minions(Id)," +
-                    "VillainId INT FOREIGN KEY REFERENCES Villains(Id)," +
-                    "CONSTRAINT PK_MinionsVillains PRIMARY KEY (MinionId, VillainId)";
-            if (!dataBaseTables.ContainsKey(tableName))
-                dataBaseTables[tableName] = null;
-            dataBaseTables[tableName] = filds;
-
-            return dataBaseTables;
-        }
+        
     }
 }
---
+++ EntityFrameworkCore/IntroductionToDBApps/IntroductionToDBApps/TablesPattern.cs
@@ -0,0 +1,63 @@
+﻿namespace IntroductionToDBApps
+{
+    using System.Collections.Generic;
+
+    public class TablesPattern
+    {
+        public static Dictionary<string, string> Tables()
+        {
+            var dataBaseTables = new Dictionary<string, string>();
+            string tableName = null;
+            string filds = null;
+
+            tableName = "Countries";
+            filds = "Id INT PRIMARY KEY IDENTITY," +
+                    "Name VARCHAR(50)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            tableName = "Towns";
+            filds = "Id INT PRIMARY KEY IDENTITY," +
+                    "Name VARCHAR(50)," +
+                    "CountryCode INT FOREIGN KEY REFERENCES Countries(Id)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            tableName = "Minions";
+            filds = "Id INT PRIMARY KEY IDENTITY," +
+                    "Name VARCHAR(30)," +
+                    "Age INT," +
+                    "TownId INT FOREIGN KEY REFERENCES Towns(Id)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            tableName = "EvilnessFactors";
+            filds = "Id INT PRIMARY KEY IDENTITY," +
+                    "Name VARCHAR(50)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            tableName = "Villains";
+            filds = "Id INT PRIMARY KEY IDENTITY," +
+                    "Name VARCHAR(50)," +
+                    "EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            tableName = "MinionsVillains";
+            filds = "MinionId INT FOREIGN KEY REFERENCES Minions(Id)," +
+                    "VillainId INT FOREIGN KEY REFERENCES Villains(Id)," +
+                    "CONSTRAINT PK_MinionsVillains PRIMARY KEY (MinionId, VillainId)";
+            if (!dataBaseTables.ContainsKey(tableName))
+                dataBaseTables[tableName] = null;
+            dataBaseTables[tableName] = filds;
+
+            return dataBaseTables;
+        }
+    }
+}
