1d343733b4136595fef4677a0bb5e0d4e5bd63df
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-07-08 20:42:44 +0300

    update code

--- EntityFrameworkCore/EntityRelations/P03_FootballBetting.Data.Models/DbConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P03_FootballBetting.Data
-{
-    internal static class DbConfiguration
-    {
-        internal static string ConnectionString =>
-            "Server=.\\SQLKARAIVANOV;Database=FootballBookmakerSystem;Integrated Security=True;";
-    }
-}
--- EntityFrameworkCore/EntityRelations/P03_FootballBetting.Data/FootballBettingContext.cs
+++ EntityFrameworkCore/EntityRelations/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -9,12 +9,35 @@
 
         public FootballBettingContext(DbContextOptions options) : base(options) { }
 
+        public DbSet<Bet> Bets { get; set; }
+
+        public DbSet<Color> Colors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Player> Players { get; set; }
+
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+
+        public DbSet<Position> Positions { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder ob)
         {
             if(!ob.IsConfigured)
                 ob.UseSqlServer(DbConfiguration.ConnectionString);
         }
 
-
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            
+        }
     }
 }
