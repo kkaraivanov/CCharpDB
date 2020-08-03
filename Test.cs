ea4fa6afc0f9cd89200a64898b55c12eeb59b922
kkaraivanov <kostadin.karaivanov@outlook.com>
2020-08-03 20:46:03 +0300

    test commiit

---
+++ Test.cs
@@ -0,0 +1,19 @@
+﻿ DateTime parsedDate;
+                var isValidDate = DateTime.TryParseExact(
+                    dto.DateTime, "yyyy-MM-dd HH:mm:ss",
+                        CultureInfo.InvariantCulture, 
+                        DateTimeStyles.None, out parsedDate);
+                if (!isValidDate)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var date = parsedDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+                
+                var projection = new Projection()
+                {
+                    Hall = context.Halls.FirstOrDefault(x => x.Id == dto.HallId),
+                    Movie = context.Movies.FirstOrDefault(x => x.Id == dto.MovieId),
+                    DateTime = DateTime.Parse(date)
+                };
